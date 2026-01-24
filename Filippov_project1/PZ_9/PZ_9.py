ebfa0ad9d3fbc6bf25bcab8b25bc0a0a054d0339
dsifjsfisjfisf <nikitafiliprov@gmail.com>
2026-01-24 17:05:44 +0300

    Update PZ_9.py

--- Filippov_project1/PZ_9/PZ_9.py
+++ Filippov_project1/PZ_9/PZ_9.py
@@ -1,1 +1,7 @@
+magnit = {"молоко", "соль", "сахар"}
+pyaterochka = {"мясо", "молоко", "сыр"}
 
+print("1. Товары из магнита, отсутсвующие в пятерочке", magnit - pyaterochka)
+print("2. Товары из Пятерочки, отсутсвующие в магните", pyaterochka - magnit)
+print("3. Полный перечень всех товаров", magnit | pyaterochka)
+print("4. Перечни товаров равны", magnit == pyaterochka)
