7d4b91aac47bc6dde022d2fbe5c94719dcadf663
dsifjsfisjfisf <nikitafiliprov@gmail.com>
2026-04-24 15:53:04 +0300

    Update PZ_12.1.py

--- Filippov_project1/PZ_12/PZ_12.1.py
+++ Filippov_project1/PZ_12/PZ_12.1.py
@@ -1,10 +1,18 @@
 # 1. В матрице найти отрицательные элементы, сформировать из них новый массив.
 # Вывести размер полученного массива.
 
-matrix = [[3, -5, 7], [-2, 4, -8], [6, -1, 9]]
+import random
+
+rows = int(input("Введите количество строк: "))
+cols = int(input("Введите количество столбцов: "))
+
+matrix = [[random.randint(-20, 20) for _ in range(cols)] for _ in range(rows)]
+
+print("Матрица:")
+for row in matrix:
+    print(row)
 
 negatives = list(filter(lambda x: x < 0, [num for row in matrix for num in row]))
 
-print("Матрица:", matrix)
-print("Отрицательные элементы:", negatives)
+print("\nОтрицательные элементы:", negatives)
 print("Размер полученного массива:", len(negatives))
