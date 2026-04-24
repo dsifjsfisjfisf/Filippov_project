0920af9af1972d980c8f1d4cca137060c2c7788b
dsifjsfisjfisf <nikitafiliprov@gmail.com>
2026-04-24 17:01:00 +0300

    Update pz_10.1.py

--- Filippov_project1/PZ_10/pz_10.1.py
+++ Filippov_project1/PZ_10/pz_10.1.py
@@ -1,23 +1,35 @@
-# 2. Из предложенного текстового файла (text18-24.txt) вывести на экран его содержимое,
-# количество символов, принадлежащих к группе букв.
-# Сформировать новый файл, в который поместить текст в стихотворной форме
-# предварительно заменив символы нижнего регистра на верхний.
+import random
 
-with open('text18-24.txt', 'r', encoding='utf-16le') as f:
-    content = f.read()
+numbers1 = [random.randint(-20, 20) for _ in range(10)]
+numbers2 = [random.randint(-20, 20) for _ in range(10)]
 
-print("Содержимое файла:")
-print(content)
+with open('first.txt', 'w', encoding='utf-16le') as f:
+    f.write(' '.join(map(str, numbers1)))
 
-letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-letter_count = 0
-for char in content:
-    if char in letters:
-        letter_count += 1
+with open('second.txt', 'w', encoding='utf-16le') as f:
+    f.write(' '.join(map(str, numbers2)))
 
-print("\nКоличество символов, принадлежащих к группе букв:", letter_count)
+multiples_3 = [x for x in numbers1 if x % 3 == 0]
+product = 1
+for x in multiples_3:
+    product *= x
+min_element = min(numbers1)
 
-with open('poetic.txt', 'w', encoding='utf-16le') as f:
-    f.write(content.upper())
+multiples_5 = [x for x in numbers2 if x % 5 == 0]
+count = len(numbers2)
+average = sum(numbers2) / count if count > 0 else 0
 
-print("\nФайл poetic.txt создан")
+with open('result.txt', 'w', encoding='utf-16le') as f:
+    f.write("Содержимое первого файла:\n")
+    f.write(' '.join(map(str, numbers1)) + "\n")
+    f.write("Элементы кратные 3: " + ' '.join(map(str, multiples_3)) + "\n")
+    f.write("Произведение элементов: " + str(product) + "\n")
+    f.write("Минимальный элемент: " + str(min_element) + "\n\n")
+    
+    f.write("Содержимое второго файла:\n")
+    f.write(' '.join(map(str, numbers2)) + "\n")
+    f.write("Элементы кратные 5: " + ' '.join(map(str, multiples_5)) + "\n")
+    f.write("Количество элементов: " + str(count) + "\n")
+    f.write("Среднее арифметическое элементов: " + str(average) + "\n")
+
+print("Файлы созданы")
