96ab846ed068ae4e959d92b2624b196e0730be04
dsifjsfisjfisf <nikitafiliprov@gmail.com>
2026-06-04 17:39:46 +0300

    Update pz_13_1.py

--- Filippov_project1/PZ_13/pz_13_1.py
+++ Filippov_project1/PZ_13/pz_13_1.py
@@ -1,1 +1,9 @@
+import re
 
+with open('Dostoevsky.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+years = re.findall(r'\b\d{4}\s+(?:года|год|году)\b', text)
+
+print(*list(map(lambda x: x, years)), sep='\n')
+print(f"\nКоличество: {len(years)}")
