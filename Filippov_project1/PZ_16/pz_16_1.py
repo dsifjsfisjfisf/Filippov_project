6ca4f7a61f565c3b35cec4518e93c6eb86e8e830
dsifjsfisjfisf <nikitafiliprov@gmail.com>
2026-06-05 15:28:20 +0300

    Update pz_16_1.py

--- Filippov_project1/PZ_16/pz_16_1.py
+++ Filippov_project1/PZ_16/pz_16_1.py
@@ -1,1 +1,22 @@
+class Computer:
+    def __init__(self, brand, processor, ram):
+        self.brand = brand
+        self.processor = processor
+        self.ram = ram
+    
+    def display_info(self):
+        print(f"Марка: {self.brand}, Процессор: {self.processor}, Оперативная память: {self.ram}")
 
+def get_input(field):
+    return input(f"Введите {field}: ")
+
+def create_computer(data):
+    return Computer(*data)
+
+def display_computer(computer):
+    computer.display_info()
+    return computer
+
+data = list(map(get_input, ["марку компьютера", "процессор", "оперативную память"]))
+pc = create_computer(data)
+display_computer(pc)
